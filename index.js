9d11fab7c210888f99253299e70cea7b885aee75
rogix <12768403+rogix@users.noreply.github.com>
2020-03-29 14:58:30 -0300

    Add middleware to validate inputs

--- index.js
+++ index.js
@@ -7,20 +7,39 @@ server.use(express.json());
 
 const users = ['Maria', 'Pedro',  'Henrique'];
 
+// Hey, I am a middleware, I can get accessed everywhere
+function checkIfUserExists(req, res, next) {
+    if (!req.body.name) {
+        return res.status(400).json({error: 'User name is required'})
+    }
+
+    return next();
+}
+
+function checkUserInArray(req, res, next) {
+    const user = users[req.params.index]
+
+    if (!user) {
+        return res.status(400).json({error: 'User does not exists'})
+    }
+
+    req.user = user;
+
+    return next();
+}
+
 // List all users
 server.get('/users', (req, res) => {
     return res.json(users);
 })
 
 // List users by id
-server.get('/users/:index', (req, res) => {
-    const {index} = req.params;
-    
-    return res.json(users[index]);
+server.get('/users/:index', checkUserInArray, (req, res) => {
+    return res.json(req.user);
 })
 
 // Create new user
-server.post('/users', (req, res) => {
+server.post('/users', checkIfUserExists, (req, res) => {
     const { name } = req.body;
 
     users.push(name);
@@ -29,7 +48,7 @@ server.post('/users', (req, res) => {
 })
 
 // Update user based on ID
-server.put('/users/:index', (req, res) => {
+server.put('/users/:index', checkIfUserExists, checkUserInArray, (req, res) => {
     const { index } = req.params;
     const { name } = req.body;
 
@@ -39,11 +58,12 @@ server.put('/users/:index', (req, res) => {
 })
 
 // Delete users based on ID
-server.delete('/users/:index', (req, res) => {
+server.delete('/users/:index', checkUserInArray, (req, res) => {
     const { index } = req.params;
 
     users.splice(index, 1);
 
     return res.json(users);
 })
+
 server.listen(3000);
